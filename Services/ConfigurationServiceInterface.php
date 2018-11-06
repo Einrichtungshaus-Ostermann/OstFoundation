43976f0b581a0f6a27dc6878d84e8a1f84e9cacf
Tim Windelschmidt <fionera@fionera.de>
2018-11-06 08:59:22 +0100

    Revert "Remove Configuration Service"
    This reverts commit 833718a

---
+++ Services/ConfigurationServiceInterface.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/**
+ * Einrichtungshaus Ostermann GmbH & Co. KG - Foundation
+ *
+ * Foundation
+ *
+ * @package   OstFoundation
+ *
+ * @author    Eike Brandt-Warneke <e.brandt-warneke@ostermann.de>
+ * @copyright 2018 Einrichtungshaus Ostermann GmbH & Co. KG
+ * @license   proprietary
+ */
+namespace OstFoundation\Services;
+
+interface ConfigurationServiceInterface
+{
+    /**
+     * ...
+     *
+     * @param string $pluginName
+     * @return array
+     */
+    public function getConfig(string $pluginName): array;
+}
