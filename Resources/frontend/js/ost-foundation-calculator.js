4bcd8276005988815f00f82d4eb6ff283ebf39e7
Eike Brandt-Warneke <e.brandt-warneke@ostermann.de>
2018-11-28 09:44:50 +0100

    added summary

--- Resources/frontend/js/ost-foundation-calculator.js
+++ Resources/frontend/js/ost-foundation-calculator.js
@@ -28,7 +28,8 @@
             operatorDecimal:  '*[data-decimal="true"]',
             operatorPercent:  '*[data-percent="true"]',
             operatorNegative: '*[data-negative="true"]',
-            output:           'button[data-output="true"]'
+            output:           'button[data-output="true"]',
+            summary:          'button[data-summary="true"]'
         },
 
         // more options...
@@ -40,7 +41,7 @@
         },
 
         // ...
-        template: '<div class="calculator-container"><button style="letter-spacing: 6px;" class="is--button is--blue is--bold is--output" data-output="true"></button><button class="is--button" data-number="true" data-input="7">7</button><button class="is--button" data-number="true" data-input="8">8</button><button class="is--button" data-number="true" data-input="9">9</button><button class="is--button" data-negative="true">+/-</button><button class="is--button" data-percent="true">%</button><button class="is--button" data-number="true" data-input="4">4</button><button class="is--button" data-number="true" data-input="5">5</button><button class="is--button" data-number="true" data-input="6">6</button><button class="is--button" data-operator="true" data-input="+">+</button><button class="is--button" data-operator="true" data-input="-">-</button><button class="is--button" data-number="true" data-input="1">1</button><button class="is--button" data-number="true" data-input="2">2</button><button class="is--button" data-number="true" data-input="3">3</button><button class="is--button" data-operator="true" data-input="*">*</button><button class="is--button" data-operator="true" data-input="/">/</button><button class="is--button" data-number="true" data-input="0">0</button><button class="is--button" data-decimal="true">,</button><button class="is--button is--green" data-operator="true" data-input="=">=</button><button class="is--button is--red" data-clear="true">C</button><button class="is--button is--red" data-clear-entry="true">CE</button></div>',
+        template: '<div class="calculator-container"><button class="is--button is--blue is--bold is--summary" data-summary="true"></button><button class="is--button is--blue is--bold is--output" data-output="true"></button><button class="is--button" data-number="true" data-input="7">7</button><button class="is--button" data-number="true" data-input="8">8</button><button class="is--button" data-number="true" data-input="9">9</button><button class="is--button" data-negative="true">+/-</button><button class="is--button" data-percent="true">%</button><button class="is--button" data-number="true" data-input="4">4</button><button class="is--button" data-number="true" data-input="5">5</button><button class="is--button" data-number="true" data-input="6">6</button><button class="is--button" data-operator="true" data-input="+">+</button><button class="is--button" data-operator="true" data-input="-">-</button><button class="is--button" data-number="true" data-input="1">1</button><button class="is--button" data-number="true" data-input="2">2</button><button class="is--button" data-number="true" data-input="3">3</button><button class="is--button" data-operator="true" data-input="*">*</button><button class="is--button" data-operator="true" data-input="/">/</button><button class="is--button" data-number="true" data-input="0">0</button><button class="is--button" data-decimal="true">,</button><button class="is--button is--green" data-operator="true" data-input="=">=</button><button class="is--button is--red" data-clear="true">C</button><button class="is--button is--red" data-clear-entry="true">CE</button></div>',
 
         // our input
         input: "",
@@ -56,6 +57,7 @@
         $clearEntryButton:  null,
         $submitButton: null,
         $output: null,
+        $summary: null,
         $input: null,
         $operator: null,
         $operatorDecimal: null,
@@ -66,6 +68,7 @@
         accum: 0,
         flagNewNum: false,
         pendingOp: "",
+        resetSummary: false,
 
         // ...
         open: function( title, options )
@@ -83,6 +86,7 @@
             me.accum = 0;
             me.flagNewNum = false;
             me.pendingOp = "";
+            me.resetSummary = false;
 
             // open a modal with our template
             $.modal.open( me.template, {
@@ -121,6 +125,7 @@
             me.$clearEntryButton = me.$el.find( me.selectors.clearEntryButton );
             me.$submitButton = me.$el.find( me.selectors.submitButton );
             me.$output = me.$el.find( me.selectors.output );
+            me.$summary = me.$el.find( me.selectors.summary );
             me.$input = me.$el.find( me.selectors.number );
             me.$operator = me.$el.find( me.selectors.operator );
             me.$operatorDecimal = me.$el.find( me.selectors.operatorDecimal );
@@ -153,15 +158,31 @@
             // get the new input
             var input = button.attr("data-input");
 
+            if ( me.resetSummary == true )
+            {
+                me.resetSummary = false;
+                me.$summary.html( "" );
+            }
+
             // new input?
             if (me.flagNewNum)
             {
                 // do it
                 me.input = input;
                 me.flagNewNum = false;
+                me.$summary.html( me.$summary.html() + input );
             }
             else
             {
+                // ...
+                if ( me.input == "0" )
+                {
+                    if ( me.input != "0" )
+                        me.$summary.html( me.$summary.html() + input );
+                }
+                else
+                    me.$summary.html( me.$summary.html() + input );
+                
                 // special case for 0 as current input
                 me.input = ( me.input == "0" )
                     ? input
@@ -186,6 +207,14 @@
             }
             else
             {
+                // do we want to reset the summary?
+                if ( me.resetSummary == true && input != "=" )
+                {
+                    // reset it
+                    me.resetSummary = false;
+                    me.$summary.html( me.$output.html() );
+                }
+
                 // not new
                 me.flagNewNum = true;
 
@@ -200,6 +229,27 @@
                     me.accum *= parseFloat(me.input);
                 else
                     me.accum = parseFloat(me.input);
+
+                // when we want the calculated result
+                if ( input == "=" )
+                    // round it
+                    me.accum = Math.round( parseFloat(me.accum) * 100 ) / 100;
+
+                // summary always for not =
+                if ( input != "=" )
+                {
+                    me.$summary.html( me.$summary.html() + input );
+                }
+                else
+                {
+                    // reset summary on next input
+                    me.resetSummary = true;
+
+                    // do we already have the "=" as last char?
+                    if ( me.$summary.html().slice(-1) != "=" )
+                        // add it if not
+                        me.$summary.html( me.$summary.html() + input );
+                }
 
                 // set output
                 me.input = me.accum;
@@ -223,9 +273,13 @@
             if (me.flagNewNum) {
                 curReadOut = "0.";
                 me.flagNewNum = false;
+                me.$summary.html( me.$summary.html() + "0." );
             } else {
                 if (curReadOut.indexOf(".") == -1)
+                {
                     curReadOut += ".";
+                    me.$summary.html( me.$summary.html() + "." );
+                }
             }
 
             // set output
@@ -239,8 +293,17 @@
             // get this
             var me = this;
 
+            // ...
+            var percent = (parseFloat(me.input) / 100) * parseFloat(me.accum);
+
+            // round it
+            percent = Math.round( percent * 100 ) / 100;
+
+            // update summary
+            me.$summary.html( me.$summary.html() + "%" );
+
             // calculate and output
-            me.input = (parseFloat(me.input) / 100) * parseFloat(me.accum);
+            me.input = percent;
             me.$output.html( me.input );
         },
 
@@ -275,8 +338,10 @@
 
             // clear
             me.input = "";
+            me.$summary.html( "" );
             me.$output.html( "" );
             me.flagNewNum = true;
+            me.resetSummary = false;
         },
 
         // ...
